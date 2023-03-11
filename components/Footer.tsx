8afb1574ab974318821d9b440dd4a6e501e75a2b
xiaotian <wanxiaotian888@gmail.com>
2023-03-11 21:15:02 +0800

    style: 修改样式

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -36,8 +36,7 @@ export const FooterImpl: React.FC = () => {
   return (
     <footer className={styles.footer}>
       <div className={styles.copyright}>
-        <span>Copyright {new Date().getFullYear()} {config.author}</span>
-        <a href="https://beian.miit.gov.cn" target="_blank" rel="noreferrer">湘ICP备2023003413号-1</a>
+        <span>Copyright {new Date().getFullYear()} {config.author}</span> <a href="https://beian.miit.gov.cn" target="_blank" rel="noreferrer">湘ICP备2023003413号-1</a>
       </div>
 
       <div className={styles.settings}>
